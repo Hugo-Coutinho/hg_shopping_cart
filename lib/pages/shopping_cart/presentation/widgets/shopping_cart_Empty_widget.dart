830fb90c7b0ebbe771db4d6d27a2a613ef5ca9f5
Rogerinho <hugocoutinho@brq.com>
2020-05-09 05:45:42 -0300

    adding lottie for animations

--- lib/pages/shopping_cart/presentation/widgets/shopping_cart_Empty_widget.dart
+++ lib/pages/shopping_cart/presentation/widgets/shopping_cart_Empty_widget.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:lottie/lottie.dart';
 
 class ShoppingCartEmptyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child: new Text("empty list"),);
+    return Center(
+      child: Lottie.asset(
+        'assets/lottie/9091-empty-sad-shopping-bag.json',
+        width: 200,
+        height: 200,
+        fit: BoxFit.fill,
+      ),
+    );
   }
 }
